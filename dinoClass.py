11629fefd102236c2fb39eca74f04801581470f4
strangy-strange <seripp28@gmail.com>
2022-04-14 14:26:03 +0300

    pushing draft

--- dinoClass.py
+++ dinoClass.py
@@ -1,24 +1,52 @@
 import cv2
+import numpy as np
 
-import numpy
-
+from random import randint
 from time import sleep
 
 
 class Rect:
-    def __init__ (self, y, x, H, W=None,\
+    def __init__ (self,cnv, y, x, H, W=None,\
                   clr=(0,255,0), bclr=(1,1,1)):
-        self.senter_y = y
-        self.senter_x = x
+        #self.senter_y = y
+        #self.senter_x = x
+        self.y = y
+        self.x = x
         self.H = int( H//2 )
         self.clr = clr
-        self.b_clr = bclr
-
+        self.back_clr = bclr
+        self.cnv = cnv
+        
         if W == None:
             self.W = int( H//2 )
         else:
             self.W = int( W//2 )
 
-    #def draw(self):
+    def draw(self):
+        self.cnv[ self.y - self.W : self.y + self.W,
+                 self.x - self.H : self.x + self.H] = self.clr
+    def destroy(self):
+        self.cnv[ self.y - self.W : self.y + self.W,
+                 self.x - self.H : self.x + self.H] = self.back_clr
 
-    ## TODO: deny from cnv in methods   
+
+
+if __name__ == '__main__':
+
+    cnv = np.ones((800, 800, 3), dtype=np.uint8() )
+
+    test_rect = Rect(cnv, 400, 400, 100, 50)
+
+    while True:
+
+        cv2.imshow('main test', cnv)
+
+        key = cv2.waitKey(1)
+
+        if key == 27:
+            break
+        if key == ord('q'):
+            test_rect.draw()
+        if key == ord('e'):
+            test_rect.destroy()
+cv2.destroyAllWindows()
